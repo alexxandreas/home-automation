cda3329141818420d92f6cfe156a310743ae4009
Alexander <alexxandreas@gmail.com>
2017-03-08 17:59:07 +0000

    -

--- dist/release/update.bash
+++ dist/release/update.bash
@@ -1,5 +1,18 @@
-rm -rf /tmp/home-automation
-git clone https://github.com/alexxandreas/home-automation.git /tmp/home-automation
-cp -rf /tmp/home-automation/dist/release/* /opt/z-way-server/automation/modules/MyHomeAutomation
-rm -rf /tmp/home-automation
-echo 1
+#!/bin/bash
+
+tmpDir=/tmp/home-automation
+destDir=/opt/z-way-server/automation/modules/MyHomeAutomation
+gitRepo=https://github.com/alexxandreas/home-automation.git
+
+
+rm -rf $tmpDir
+git clone $gitRepo $tmpDir
+cp -rf $tmpDir/dist/release/* $destDir
+
+rm -f $destDir/update.bash.bak
+mv $destDir/update.bash $destDir/update.bash.bak
+cp -f $tmpDir/dist/release/update.bash $destDir/update.bash
+
+rm -rf $tmpDir
+
+echo echo-1
