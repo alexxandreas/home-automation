57592d1d65b2304fc0beb26ffd4332315c54adf3
Alexander <alexxandreas@gmail.com>
2017-03-08 17:47:45 +0000

    -

--- src/update.bash
+++ src/update.bash
@@ -1,4 +1,4 @@
 rm -rf /tmp/home-automation
 git clone https://github.com/alexxandreas/home-automation.git /tmp/home-automation
-cp -rf /tmp/home-automation/dist/release /opt/z-way-server/automation/modules/MyHomeAutomation
+cp -rf /tmp/home-automation/dist/release/* /opt/z-way-server/automation/modules/MyHomeAutomation
 rm -rf /tmp/home-automation
