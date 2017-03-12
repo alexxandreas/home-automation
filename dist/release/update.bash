5f6504644fcdba7e3bdc2534d1a47ad14c55be12
Alexander <alexxandreas@gmail.com>
2017-03-12 18:52:42 +0000

    -

--- dist/release/update.bash
+++ dist/release/update.bash
@@ -5,20 +5,21 @@ destDir=/opt/z-way-server/automation/modules/MyHomeAutomation
 gitRepo=https://github.com/alexxandreas/home-automation.git
 
 if [ ! -d $tmpDir ]; then
-    # Control will enter here if $DIRECTORY doesn't exist.
-    echo "Temp directory doesn't exist"
+    echo "Temp directory doesn't exist. Start git clone"
     git clone $gitRepo $tmpDir
 else
-    
+    echo "Temp directory doesn't exist. Start git pull"
+    git -C $tmpDir pull
 fi
 
-rm -rf $tmpDir
-git clone $gitRepo $tmpDir
+#rm -rf $tmpDir
+#git clone $gitRepo $tmpDir
+
 cp -rf $tmpDir/dist/release/* $destDir
 
 rm -f $destDir/update.bash.bak
 mv $destDir/update.bash $destDir/update.bash.bak
 cp -f $tmpDir/dist/release/update.bash $destDir/update.bash
 
-rm -rf $tmpDir
+#rm -rf $tmpDir
 
