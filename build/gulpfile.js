19bfebccc3a4d6076202aa2af2f10ddbf4e3094c
Alexander <alexxandreas@gmail.com>
2017-03-19 18:27:28 +0000

    -

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -77,7 +77,7 @@ var copyToRootFiles = [
     //'../src/update.bash'
     '../src/**',
     '!../src/modules/*/htdocs/**/*.html',
-    '../src/modules/*/htdocs/**/index.html',
+    '../src/modules/*/htdocs/index.html',
     '!../src/modules/*/htdocs/**/*.js',
     '!../src/modules/*/htdocs/**/*.css'
     ];
