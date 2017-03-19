9bdc127f9a6a9471a0b49ad6ff1f89766f25d3c3
Alexander <alexxandreas@gmail.com>
2017-03-19 17:33:16 +0000

    -

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -38,13 +38,15 @@ var libsJs = [
 	'./node_modules/angular-aria/angular-aria.js',
 	'./node_modules/angular-material/angular-material.js',
 	'./node_modules/md-data-table/dist/md-data-table.js',
-	'./node_modules/md-data-table/dist/md-data-table-templates.js'
+	'./node_modules/md-data-table/dist/md-data-table-templates.js',
+	'./node_modules/angular-material-icons/angular-material-icons.js'
 ];
 	
 // css-sы, используемые на фронте
 var libsCss = [
 	'./node_modules/angular-material/angular-material.css',
-	'./node_modules/md-data-table/dist/md-data-table-style.js'
+	'./node_modules/md-data-table/dist/md-data-table-style.js',
+	'./node_modules/angular-material-icons/angular-material-icons.css'
 ];
 	
 // JS клиентского приложения
